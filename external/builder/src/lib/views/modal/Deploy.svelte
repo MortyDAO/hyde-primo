2a0c38bf9c4770d0dfbefcbfa00ada8f7f45db67
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2023-10-18 14:58:50 +0900

    Apply the patch

--- external/builder/src/lib/views/modal/Deploy.svelte
+++ external/builder/src/lib/views/modal/Deploy.svelte
@@ -20,9 +20,11 @@
 	import { push_site, build_site_bundle } from './Deploy'
 	import PrimaryButton from '$lib/ui/PrimaryButton.svelte'
 	import { dataChanged } from '$lib/database'
+	import { content, primary_language } from '$lib/stores/data/site.js'
+	import { Language_Name } from '$lib/const.js'
 
 	let stage = 'INITIAL'
-	let primary_language = 'en'
+	$: site_languages = $content ? Object.keys($content) : []
 
 	if ($active_deployment) {
 		stage = 'CONNECT_REPO__ACTIVE'
@@ -62,7 +64,7 @@
 		const files = await build_site_bundle({
 			pages: $pages,
 			symbols: $symbols,
-			primary_language
+			primary_language: $primary_language
 		})
 		if (!files) {
 			loading = false
@@ -97,7 +99,7 @@
 		} else {
 			destination_repo = $active_deployment.repo.full_name
 		}
-		const deployment = await push_site(destination_repo, creating_new_repo, primary_language)
+		const deployment = await push_site(destination_repo, creating_new_repo, $primary_language)
 		if (deployment) {
 			$active_deployment = deployment
 			stage = 'CONNECT_REPO__ACTIVE__SUCCESS'
@@ -169,13 +171,6 @@
 						<span>Connect to Github</span>
 					</button>
 				{/if}
-			</div>
-			<div style="display: flex; justify-content: flex-end; align-items: center; gap: 1rem;">
-				<div>Primary Language:</div>
-				<select class="primo-button" bind:value={primary_language}>
-					<option value="en">English</option>
-					<option value="ja">Japanese</option>
-				</select>
 			</div>
 		</div>
 	{:else if stage === 'CONNECT_GITHUB'}
@@ -305,6 +300,16 @@
 			{/if}
 		</div>
 	{/if}
+	<div class="container">
+		<div style="display: flex; justify-content: flex-end; align-items: center; gap: 1rem;">
+			<div>Primary Language:</div>
+			<select class="primo-button" bind:value={$primary_language}>
+				{#each site_languages as lang_id}
+					<option value={lang_id}>{Language_Name(lang_id)}</option>
+				{/each}
+			</select>
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
